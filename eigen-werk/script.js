40891babcb25afd1f1cbc06c613276e03daed716
DeBosOs <luc.hoogerwerf@hva.nl>
2025-02-03 15:13:28 +0100

    niet het mooiste... script in html...

--- eigen-werk/script.js
+++ eigen-werk/script.js
@@ -1,33 +1,45 @@
-function speelNoot() {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    
-    const nootFrequentie = parseFloat(document.getElementById("noot").value);
-    const sampleRate = audioContext.sampleRate;
-    const bufferSize = Math.round(sampleRate / nootFrequentie);
-    
-    const buffer = audioContext.createBuffer(1, bufferSize, sampleRate);
-    const data = buffer.getChannelData(0);
+// import * as Tone from "tone";
 
-    // ruis weg halen
-    for (let i = 0; i < bufferSize; i++) {
-        data[i] = (Math.random() * 2 - 1) * 0.5;
-    }
+// // Synthesizer maken
+// const synth = new Tone.Synth().toDestination();
 
-    const filter = audioContext.createBiquadFilter();
-    filter.type = "lowpass";
-    filter.frequency.setValueAtTime(3000, audioContext.currentTime);
+// // Event listener voor de button
+// document.getElementById("playButton").addEventListener("click", async () => {
+//     await Tone.start(); // Nodig om audio-context te starten in browsers
+//     synth.triggerAttackRelease("C4", "8n");
+// });
 
-    const bufferSource = audioContext.createBufferSource();
-    bufferSource.buffer = buffer;
-    bufferSource.loop = true;
+
+// function speelNoot() {
+//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     
-    bufferSource.connect(filter);
-    filter.connect(audioContext.destination);
+//     const nootFrequentie = parseFloat(document.getElementById("noot").value);
+//     const sampleRate = audioContext.sampleRate;
+//     const bufferSize = Math.round(sampleRate / nootFrequentie);
     
-    bufferSource.start();
+//     const buffer = audioContext.createBuffer(1, bufferSize, sampleRate);
+//     const data = buffer.getChannelData(0);
+
+//     // ruis weg halen
+//     for (let i = 0; i < bufferSize; i++) {
+//         data[i] = (Math.random() * 2 - 1) * 0.5;
+//     }
+
+//     const filter = audioContext.createBiquadFilter();
+//     filter.type = "lowpass";
+//     filter.frequency.setValueAtTime(3000, audioContext.currentTime);
+
+//     const bufferSource = audioContext.createBufferSource();
+//     bufferSource.buffer = buffer;
+//     bufferSource.loop = true;
     
-    // tijd
-    setTimeout(() => {
-        bufferSource.stop();
-    }, 1000);
-}
+//     bufferSource.connect(filter);
+//     filter.connect(audioContext.destination);
+    
+//     bufferSource.start();
+    
+//     // tijd
+//     setTimeout(() => {
+//         bufferSource.stop();
+//     }, 1000);
+// }
