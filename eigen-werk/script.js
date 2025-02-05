051741b9366d21d3d3dd017bcf86c19c83571240
DeBosOs <luc.hoogerwerf@hva.nl>
2025-02-05 12:57:58 +0100

    Holy shit, dit werkt!. Thanks Dion ;)

--- eigen-werk/script.js
+++ eigen-werk/script.js
@@ -1,45 +1,18 @@
-// import * as Tone from "tone";
+// // Maak de reverb met de juiste manier in v15.x
+const reverb = new Tone.Reverb(2);  // Je kunt hier de decay tijd instellen
+reverb.toDestination();  // Stuur het naar de bestemming (je speakers)
 
-// // Synthesizer maken
-// const synth = new Tone.Synth().toDestination();
+// // Maak de synth en verbind deze met de reverb
+const synth = new Tone.Synth().connect(reverb);
 
-// // Event listener voor de button
-// document.getElementById("playButton").addEventListener("click", async () => {
-//     await Tone.start(); // Nodig om audio-context te starten in browsers
-//     synth.triggerAttackRelease("C4", "8n");
-// });
+const notes = document.querySelectorAll('.note');
+notes.forEach((n) => {
+    n.addEventListener('click', () => {
+        synth.triggerAttackRelease(n.getAttribute('data-note'), "8n");
+    })
+})
 
-
-// function speelNoot() {
-//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    
-//     const nootFrequentie = parseFloat(document.getElementById("noot").value);
-//     const sampleRate = audioContext.sampleRate;
-//     const bufferSize = Math.round(sampleRate / nootFrequentie);
-    
-//     const buffer = audioContext.createBuffer(1, bufferSize, sampleRate);
-//     const data = buffer.getChannelData(0);
-
-//     // ruis weg halen
-//     for (let i = 0; i < bufferSize; i++) {
-//         data[i] = (Math.random() * 2 - 1) * 0.5;
-//     }
-
-//     const filter = audioContext.createBiquadFilter();
-//     filter.type = "lowpass";
-//     filter.frequency.setValueAtTime(3000, audioContext.currentTime);
-
-//     const bufferSource = audioContext.createBufferSource();
-//     bufferSource.buffer = buffer;
-//     bufferSource.loop = true;
-    
-//     bufferSource.connect(filter);
-//     filter.connect(audioContext.destination);
-    
-//     bufferSource.start();
-    
-//     // tijd
-//     setTimeout(() => {
-//         bufferSource.stop();
-//     }, 1000);
-// }
+// // Functie om een noot af te spelen
+function playNote(note) {
+  
+}
